f10d38c9365ce728bc7be5f8bb72dd657677f3fc
SmeshMike <smeshmike@yandex.ru>
2022-04-16 21:33:10 +0300

    Change architecture for easier testing

--- sql_task.py
+++ sql_task.py
@@ -2,41 +2,31 @@ import sqlite3
 from sqlite3 import Cursor
 
 
-def connect_graceful(func: callable) -> callable:
-    """Decorator for graceful connection to database."""
+def connect_gracefully(func: callable) -> callable:
+    """Декоратор для безопасного подключения к бд."""
 
-    def inner():
+    def inner(**kwargs):
         with sqlite3.connect("sqlite_python.db") as connection:
             cursor = connection.cursor()
-            return func(cursor)
+            kwargs["cursor"] = cursor
+            return func(**kwargs)
 
     return inner
 
 
-@connect_graceful
-def fill_db(cursor: Cursor) -> None:
+@connect_gracefully
+def execute_functions(cursor: Cursor) -> callable:
     """
-    Fulfilling database function.
+    Функция запускающая функционал модуля.
 
-    :param cursor: database connection cursor
+    :param cursor: курсор подключения к базе данных
     """
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS clients
-        (client_id INTEGER PRIMARY KEY AUTOINCREMENT, client_name CHAR);"""
-    )
-
-    cursor.execute(
-        """INSERT INTO clients (client_name)
-        VALUES ('Иван'), ('Константин'), ('Дмитрий'), ('Александр');
-        """
-    )
-
-    cursor.connection.commit()
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS products
-        (product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name CHAR, price REAL);"""
-    )
+    users_list = [
+        ("Иван",),
+        ("Константин",),
+        ("Дмитрий",),
+        ("Александр",),
+    ]
 
     products_list = [
         ("Мяч", 299.99),
@@ -46,10 +36,47 @@ def fill_db(cursor: Cursor) -> None:
         ("Телефон", 9999.9),
         ("Кофе", 159),
     ]
+
+    orders_client_list = [2, 2, 2, 1, 1, 1, 1, 4, 3, 3, 1]
+    orders_product_list = [2, 5, 1, 1, 3, 6, 2, 5, 6, 3, 5]
+    orders_name_list = [f"Закупка {i}" for i in range(1, 12)]
+    orders_list = list(zip(orders_client_list, orders_product_list, orders_name_list))
+
+    fill_db(users=users_list, products=products_list, orders=orders_list, cursor=cursor)
+    print(return_clients_with_purchases_sum(cursor))
+    print(return_clients_who_bought_phone(cursor))
+    print(return_customs_count_by_name(cursor))
+
+
+def fill_db(users: list[tuple], products: list[tuple], orders: list[tuple], cursor: Cursor) -> None:
+    """
+    Функция заполняющая базу данных.
+
+    :param users: объекты на заполнения таблицы clients
+    :param products: объекты на заполнения таблицы products
+    :param orders: объекты на заполнения таблицы orders
+    :param cursor: курсор подключения к базе данных
+    """
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS clients
+        (client_id INTEGER PRIMARY KEY AUTOINCREMENT, client_name CHAR);"""
+    )
+
     cursor.executemany(
-        """INSERT INTO products(product_name, price) VALUES (?, ?);
-        """,
-        products_list,
+        """INSERT INTO clients(client_name) VALUES (?);""",
+        users,
+    )
+
+    cursor.connection.commit()
+
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS products
+        (product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name CHAR, price REAL);"""
+    )
+
+    cursor.executemany(
+        """INSERT INTO products(product_name, price) VALUES (?, ?);""",
+        products,
     )
 
     cursor.connection.commit()
@@ -63,29 +90,22 @@ def fill_db(cursor: Cursor) -> None:
         ON DELETE CASCADE,
         CONSTRAINT fk_product FOREIGN KEY (product_id)
         REFERENCES products (product_id)
-        ON DELETE CASCADE
-        );"""
+        ON DELETE CASCADE);"""
     )
 
-    orders_client_list = [2, 2, 2, 1, 1, 1, 1, 4, 3, 3, 1]
-    orders_product_list = [2, 5, 1, 1, 3, 6, 2, 5, 6, 3, 5]
-    orders_name_list = [f"Закупка {i}" for i in range(1, 12)]
-    orders_list = list(zip(orders_client_list, orders_product_list, orders_name_list))
     cursor.executemany(
-        """INSERT INTO orders(client_id, product_id, order_name) VALUES (?, ?, ?);
-        """,
-        orders_list,
+        """INSERT INTO orders(client_id, product_id, order_name) VALUES (?, ?, ?);""",
+        orders,
     )
     cursor.connection.commit()
 
 
-@connect_graceful
-def show_clients_with_purchases_sum(cursor: Cursor) -> list[tuple]:
+def return_clients_with_purchases_sum(cursor: Cursor) -> list[tuple]:
     """
-    Shows clients with a sum of their purchases.
+    Возвращает список клиентов с общей суммой их покупки.
 
-    :param cursor: database connection cursor
-    :returns: list of clients
+    :param cursor: курсор подключения к базе данных
+    :returns: список клиентов с общей суммой их покупки
     """
     cursor.execute(
         """SELECT client_name, SUM(price)
@@ -99,13 +119,12 @@ def show_clients_with_purchases_sum(cursor: Cursor) -> list[tuple]:
     return cursor.fetchall()
 
 
-@connect_graceful
-def show_clients_who_bought_phone(cursor: Cursor) -> list[tuple]:
+def return_clients_who_bought_phone(cursor: Cursor) -> list[tuple]:
     """
-    Shows clients who bought phone.
+    Возвращает список клиентов, которые купили телефон.
 
-    :param cursor: database connection cursor
-    :returns: list of clients
+    :param cursor: курсор подключения к базе данных
+    :returns: список клиентов, которые купили телефон
     """
     cursor.execute(
         """SELECT client_name
@@ -119,13 +138,12 @@ def show_clients_who_bought_phone(cursor: Cursor) -> list[tuple]:
     return cursor.fetchall()
 
 
-@connect_graceful
-def show_customs_count_by_name(cursor: Cursor) -> list[tuple]:
+def return_customs_count_by_name(cursor: Cursor) -> list[tuple]:
     """
-    Shows count of bought purchases by purchase name.
+    Возвращает список товаров с количеством их заказа.
 
-    :param cursor: database connection cursor
-    :returns: list of purchases with count
+    :param cursor: курсор подключения к базе данных
+    :returns: список товаров с количеством их заказа
     """
     cursor.execute(
         """SELECT product_name, COUNT(product_name)
@@ -138,7 +156,4 @@ def show_customs_count_by_name(cursor: Cursor) -> list[tuple]:
 
 
 if __name__ == "__main__":
-    fill_db()
-    print(show_clients_with_purchases_sum())
-    print(show_clients_who_bought_phone())
-    print(show_customs_count_by_name())
+    execute_functions()
